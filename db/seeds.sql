0a15bb5ca2d19dec03018fa16a5a8a7c4483ad01
Hazel Reyes <hazelnoriereyes@gmail.com>
2020-03-31 11:21:00 -0700

    added sql files and/inside db folder

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('bacon cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('double bacon cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('chili cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('healthnut burger', true);
+INSERT INTO burgers (burger_name) VALUES ('peanut butter burger');
